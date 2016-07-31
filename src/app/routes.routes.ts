c00c8eb16c13056606ca3c28327bb0cad1290b01
Jon Borgonia <remix@Titan.local>
2016-07-31 15:36:04 -0700

    removing actual project for testing

--- src/app/routes.routes.ts
+++
@@ -1,9 +0,0 @@
-import { EmailFormComponent } from './email-form';
-import { AircraftSelectionComponent } from './aircraft-selection';
-import { ThankYouComponent } from './thank-you';
-
-export const Routes = [
-  { path: '', component: EmailFormComponent },
-  { path: 'choose-your-aircraft', component: AircraftSelectionComponent },
-  { path: 'thank-you', component: ThankYouComponent }
-]
